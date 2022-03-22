0e4dc90fbb91b73b63afe528080b0eff9bc889f6
Zhang <89087596+xiaoyuzhang668@users.noreply.github.com>
2022-03-22 18:31:33 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Course File Repository for Introduction to Web Development
+
+The files in this repository are provided for use in the online course, Introduction to Web Development, offered through The Software Guild, a Wiley brand.
+
+For more information about this course or other courses we offer, please visit our website at [TheSoftwareGuild.com](https://TheSoftwareGuild.com).
+
+![The Software Guild](https://the-software-guild.s3.amazonaws.com/tsg-logos/swg_logo-black.png)
---
+++ javascript/practice/04-functions/your-code.js
@@ -0,0 +1,66 @@
+// These JavaScript comments are instructions.
+// Follow them step by step.
+// When asked, write code to complete the task described.
+
+// 1. ADD TWO NUMBERS
+// ==================
+// Complete the following function. 
+// Write code to add firstNumber and secondNumber and then return the result.
+
+function addTwoNumbers(firstNumber, secondNumber) {
+    // your code goes here
+}
+
+// Open index.html in your browser, open the JavaScript console, confirm the result.
+
+console.log("addTwoNumbers =====");
+var result = addTwoNumbers(2, 2);
+console.log("2 + 2 -> expected: 4, actual: %s", result);
+result = addTwoNumbers(33, 44);
+console.log("33 + 44 -> expected: 77, actual: %s", result);
+result = addTwoNumbers(112, 0);
+console.log("112 + 0 -> expected: 112, actual: %s", result);
+result = addTwoNumbers(-53, 40);
+console.log("-53 + 40 -> expected: -13, actual: %s", result);
+
+// 2. FIRST LETTER IN A PHRASE 
+//   (first character in a string)
+// =============================
+// Complete the following function.
+// You can assume text is a string with at least one character.
+// Write code to return the first character in text.
+// Hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String#Character_access
+function getFirstCharacter(text) {
+    // your code goes here.
+}
+
+console.log("getFirstCharacter =====");
+console.log("'strawberry' -> expected: s, actual: %s.", getFirstCharacter("strawberry"));
+console.log("'above the sea' -> expected: a, actual: %s.", getFirstCharacter("above the sea"));
+console.log("'Starling' -> expected: S, actual: %s.", getFirstCharacter("Starling"));
+console.log("'Olympus Mons' -> expected: O, actual: %s.", getFirstCharacter("Olympus Mons"));
+
+// 3. MAX
+// =======
+// Create a function named 'max' that takes three numeric parameters.
+// The function should return the maximum of the three parameters.
+// Hint: use conditional statements (if/else) or Math functions inside your function to calculate
+// the correct result.
+// Write your code here:
+
+// Uncomment the code below to verify your function is working.
+// console.log("max =====");
+// console.log("max(-1, 0, 1) -> expected: 1, actual: %s", max(-1, 0, 1));
+// console.log("max(11, 2, 1) -> expected: 11, actual: %s", max(11, 2, 1));
+// console.log("max(-15.67, 33.293, 0.51) -> expected: 33.293, actual: %s", max(-15.67, 33.293, 0.51));
+// console.log("max(4, 987654321, 0) -> expected: 987654321, actual: %s", max(4, 987654321, 0));
+
+
+
+
+
+
+/**********************************
+ * The Software Guild
+ * Copyright (C) 2019 Wiley edu LLC - All Rights Reserved
+ *********************************/
